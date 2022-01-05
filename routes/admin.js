e9fd0e5efc9407ec0b0cae361db9760f642e82cd
naimulnobel <nobelnaimulislam@gmail.com>
2022-01-05 11:06:06 +0600

    code formate

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
-const path=require("path")
-const express =require('express');
-const rootDir=require('../util/path')
-const router=express.Router()
-router.get('/add-product',(req, res,next)=> {
+const path = require("path")
+const express = require('express');
+const rootDir = require('../util/path')
+const router = express.Router()
+router.get('/add-product', (req, res, next) => {
     console.log('yo')
-    res.sendFile(path.join(rootDir,'views','addProduct.html'))
+    res.sendFile(path.join(rootDir, 'views', 'addProduct.html'))
 });
-router.post('/add-product',(req, res,next)=> {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/')
 })
-module.exports=router
+module.exports = router
